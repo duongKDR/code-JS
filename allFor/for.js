c6ea02823cc8099976402183fbbd4997ab0f4c97
KRD <duongyang@gmail.com>
2023-01-31 17:01:01 +0700

    duong

---
+++ allFor/for.js
@@ -0,0 +1,9 @@
+var majorityElement = function(nums) {
+    var nums = [3,2,3]  
+ for (let index = 0; index < nums.length; index++) {
+    const element = array[index]+ 1;
+  
+     }
+    return index
+};
+ majorityElement();
---
+++ khaiNiem/CodeKN.js
@@ -0,0 +1,14 @@
+// "I am a string";
+// 'I am also a string';
+
+// 42;
+
+// true;
+// false;
+
+
+var a = "42";
+var b = Number( a );
+
+console.log( a );	// "42"
+console.log( b );	// 42
