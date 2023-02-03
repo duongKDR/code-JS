2524eea8c26f4cdcbbe7e8417ab3f8a4562ce9b3
KRD <duongyang@gmail.com>
2023-02-03 16:34:52 +0700

    q

--- Object/toString.js
+++ Object/toString.js
@@ -1,7 +1,7 @@
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
 let text = fruits.toString();
 console.log(text);
-var me = {[]
+var me = {
     name:'duong',
     age: 19,
     address: 'TH',
